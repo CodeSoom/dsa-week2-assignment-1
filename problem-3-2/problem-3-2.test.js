74e8b11580db4020e92a493b02051f7a61131b02
hayasha <hayashasong@gmail.com>
2023-08-14 20:33:49 +0900

    Solve dsa-week2-assignment1-problem3-2
    -

--- problem-3-2/problem-3-2.test.js
+++ problem-3-2/problem-3-2.test.js
@@ -1,4 +1,19 @@
+const { Queue } = require('../problem-3-1/problem-3-1.test.js');
+
 const solution = (N, M) => {
+    const queue = new Queue();
+    for (let i = 1; i <= N; i++) {
+        queue.enqueue(i);
+    }
+
+    while (queue.size() > 1) {
+        for (let i = 0; i < (M - 1); i++) {
+            queue.enqueue(queue.dequeue());
+        }
+        queue.dequeue();
+    }
+
+    return queue.dequeue();
 };
 
 test('N명의 사람이 있을 때 M번째 사람을 없앨 때 마지막에 죽는 사람의 순서를 반환한다', () => {
