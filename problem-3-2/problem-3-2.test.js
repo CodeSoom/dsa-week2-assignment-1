4de31e9053181e6cce0220af3e4a54ec724b17ac
jihyun-jeon <jhplus13@naver.com>
2023-08-19 17:54:03 +0900

    3-2완료

--- problem-3-2/problem-3-2.test.js
+++ problem-3-2/problem-3-2.test.js
@@ -1,4 +1,15 @@
 const solution = (N, M) => {
+  let arr = new Array(N).fill(null);
+  arr = arr.map((_, i) => i + 1);
+
+  while (true) {
+    arr.splice(M - 1, 1);
+
+    if (arr.length === 2) {
+      return arr[1];
+    }
+    arr = [...arr.slice(M - 1), ...arr.slice(0, M - 1)];
+  }
 };
 
 test('N명의 사람이 있을 때 M번째 사람을 없앨 때 마지막에 죽는 사람의 순서를 반환한다', () => {
