678ecf238383c479ea17628ee3eb7a06b128d138
seungwoo Kim <kyh196201@gmail.com>
2023-09-16 23:32:06 +0900

    problem-2-1 동적 크기 변경 기능 구현

--- problem-2-1/problem-2-1.test.js
+++ problem-2-1/problem-2-1.test.js
@@ -1,7 +1,18 @@
 class Stack {
   #n = -1;
 
-  #items = [];
+  #items;
+
+  #capacity;
+
+  constructor(capacity = 0) {
+    this.#capacity = capacity;
+    this.#items = Array(capacity).fill(undefined);
+  }
+
+  isFull() {
+    return this.size() === this.#capacity;
+  }
 
   isEmpty() {
     return this.#n === -1;
@@ -12,12 +23,18 @@ class Stack {
   }
 
   push(item) {
+    if (this.isFull()) {
+      const newItems = Array(this.#capacity).fill(undefined);
+      this.#items = this.#items.concat(newItems);
+      this.#capacity *= 2;
+    }
+
     this.#n += 1;
     this.#items[this.#n] = item;
   }
 
   pop() {
-    if (this.size() === 0) {
+    if (this.isEmpty()) {
       throw new Error('스택이 비어있습니다');
     }
 
@@ -28,7 +45,17 @@ class Stack {
 
     this.#n -= 1;
 
+    if (this.size() === (this.#capacity / 4)) {
+      this.#capacity /= 2;
+
+      this.#items = this.#items.slice(0, this.#capacity);
+    }
+
     return popped;
+  }
+
+  get length() {
+    return this.#capacity;
   }
 
   [Symbol.iterator]() {
@@ -129,4 +156,34 @@ test('스택은 역순으로 순회한다', () => {
   }
 
   expect(output.reverse()).toEqual(data);
+});
+
+test('스택에 더이상 추가할 수 없을 경우 크기가 2배로 늘어난다', () => {
+  const data = ['D', 'S', 'A', 'E'];
+
+  const capacity = 3;
+  const stack = new Stack(capacity);
+
+  data.forEach((i) => {
+    stack.push(i);
+  });
+
+  expect(stack.length).toBe((2 * capacity));
+});
+
+test('아이템을 제거할 때 아이템의 수가 1 / 4이하라면 배열의 크기가 1/2로 줄어든다', () => {
+  const data = ['D', 'S', 'A', 'E'];
+
+  const capacity = 4;
+  const stack = new Stack(capacity);
+
+  data.forEach((i) => {
+    stack.push(i);
+  });
+
+  stack.pop();
+  stack.pop();
+  stack.pop();
+
+  expect(stack.length).toBe((capacity / 2));
 });
