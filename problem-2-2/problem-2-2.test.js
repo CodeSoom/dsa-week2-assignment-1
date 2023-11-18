cca10a3ebebd8bc1329faa8678ddf6dcd338ae03
syaring <sarah.k.serim@gmail.com>
2023-11-18 18:47:10 +0900

    week2 2-2: Stack 활용 문제 구현

--- problem-2-2/problem-2-2.test.js
+++ problem-2-2/problem-2-2.test.js
@@ -1,4 +1,107 @@
+class Node {
+  item;
+  next;
+}
+
+class Stack {
+  _n;
+  _first;
+
+  constructor() {
+    this._n = 0;
+  }
+
+  push(item) {
+    const newItem = new Node();
+
+    if (this.isEmpty()) {
+      this._first = newItem;
+      this._first.item = item;
+    } else {
+      const oldFirst = this._first;
+
+      this._first = newItem;
+      this._first.item = item;
+      this._first.next = oldFirst;
+    }
+
+    this._n = this._n + 1;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error('스택이 비어있습니다');
+    }
+
+    const target = this._first;
+
+    if (this._first.next) {
+      this._first = this._first.next;
+    } else {
+      this._first = undefined;
+    }
+
+    this._n = this._n - 1;
+
+    return target.item;
+  }
+
+  isEmpty() {
+    return this._n === 0;
+  }
+
+  size() {
+    return this._n;
+  }
+
+  [Symbol.iterator]() {
+    let cur = this._first;
+
+    return {
+      next() {
+        if (cur) {
+          const value = cur.item;
+
+          cur = cur.next;
+
+          return { done: false, value };
+        }
+
+        return { done: true };
+      }
+    };
+  }
+}
+
 const solution = (string) => {
+  const stack = new Stack();
+
+  const open = ['{', '(', '[']; // open 일때 push
+  const pair = { // close 일때 pop
+    '}': '{',
+    ')': '(',
+    ']': '[',
+  };
+
+  for (const str of string) {
+    const isOpenBracket = open.includes(str);
+
+    if (isOpenBracket) {
+      stack.push(str);
+    } else {
+      try {
+        const popped = stack.pop();
+
+        if (!pair[str] === popped) {
+          return false;
+        }
+      } catch(e) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 };
 
 test('문자열에 포함된 괄호의 짝이 맞을 때 true를 반환한다', () => {
