6da170b28eeb3644f4f1e33a79b09b9fd750ba84
bereal1995 <gygud98@gmail.com>
2023-12-18 06:11:06 +0900

    큐 구현하기

--- problem-3-1/problem-3-1.test.js
+++ problem-3-1/problem-3-1.test.js
@@ -1,10 +1,86 @@
+class Node {
+  #item;
+  #next;
+}
+
 class Queue {
+  #first;
+  #last;
+  #n;
+
+  constructor() {
+    this.#n = 0;
+  }
+
+  enqueue(item) {
+    const oldLast = this.#last;
+
+    this.#last = new Node();
+    this.#last.item = item;
+
+    if (this.isEmpty()) {
+      this.#first = this.#last;
+    } else {
+      oldLast.next = this.#last;
+    }
+    this.#n++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      throw new Error('큐가 비어있습니다');
+    }
+
+    const item = this.#first.item;
+    this.#first = this.#first.next;
+
+    if (this.isEmpty()) {
+      this.#last = undefined;
+    }
+
+    this.#n--;
+
+    return item;
+  }
+
+  isEmpty() {
+    return this.#n === 0;
+  }
+
+  size() {
+    return this.#n;
+  }
+
+  [Symbol.iterator]() {
+    let current = this.#first;
+
+    return {
+      next: () => {
+        if (current === undefined) {
+          return { done: true };
+        }
+
+        const value = current.item;
+        current = current.next;
+
+        return { value, done: false };
+      },
+    };
+  }
 }
 
 test('큐를 생성하면 비어있다', () => {
   const queue = new Queue();
 
   expect(queue.isEmpty()).toEqual(true);
+});
+
+test('큐가 비어있지 않으면 false를 반환한다.', () => {
+  const queue = new Queue();
+
+  queue.enqueue('D');
+
+  expect(queue.isEmpty()).toEqual(false);
 });
 
 test('큐에 값을 추가하면 개수가 증가한다', () => {
