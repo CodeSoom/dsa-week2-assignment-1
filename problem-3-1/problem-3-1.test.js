4d40e62842d060cae5983dca41559b8975da9a44
whoodongpyo <vvhooping@gmail.com>
2024-01-20 17:46:01 +0900

    refactor: dequeue 메서드 수정  - dequeue() 실행 후 queue 에 아이템이 남아 있지 않다면 #last 는 undefined 를 가리켜야 한다.

--- problem-3-1/problem-3-1.test.js
+++ problem-3-1/problem-3-1.test.js
@@ -45,9 +45,22 @@ class Queue {
     const firstItem = this.#first.item;
     this.#first = this.#first.next;
 
+    // todo: 아이템 개수를 먼저 줄인 다음, isEmpty() 를 확인해서 #last 를 undefined 처리한다.
     this.#numberOfItems -= 1;
 
+    if (this.isEmpty()) {
+      this.#last = undefined;
+    }
+
     return firstItem;
+  }
+
+  first() {
+    return this.#first;
+  }
+
+  last() {
+    return this.#last;
   }
 
   [Symbol.iterator]() {
@@ -100,7 +113,7 @@ test('큐에서 요소를 제거하면 개수가 감소한다', () => {
   expect(newSize - oldSize).toEqual(-1);
 });
 
-test('가장 나중에 삽입한게 먼저 나온다', () => {
+test('가장 먼저 삽입한게 먼저 나온다', () => {
   const queue = new Queue();
 
   queue.enqueue('D');
@@ -110,6 +123,24 @@ test('가장 나중에 삽입한게 먼저 나온다', () => {
   expect(queue.dequeue()).toBe('D');
   expect(queue.dequeue()).toBe('S');
   expect(queue.dequeue()).toBe('A');
+
+  expect(queue.first()).toBeUndefined();
+  expect(queue.last()).toBeUndefined();
+});
+
+test('아이템을 모두 제거하면 first, last 는 모두 undefined 를 가리켜야 한다.', () => {
+  const queue = new Queue();
+
+  queue.enqueue('D');
+  queue.enqueue('S');
+  queue.enqueue('A');
+
+  expect(queue.dequeue()).toBe('D');
+  expect(queue.dequeue()).toBe('S');
+  expect(queue.dequeue()).toBe('A');
+
+  expect(queue.first()).toBeUndefined();
+  expect(queue.last()).toBeUndefined();
 });
 
 test('큐이 비어있는데 dequeue을 하면 예외를 던진다', () => {
